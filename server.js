65963c0084a7abccc1b24f29fdff233ace2fef1c
Avryl Rodrigues <avryl.r123@gmail.com>
2025-11-29 11:32:26 +0400

    updated files

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 var path = require("path");
-
 const app = express();
-
 const cors = require('cors');
 
+// Enable CORS for requests from the frontend
 app.use(cors({
   origin: 'https://avrylrodrigues.github.io',
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
+// Set headers for CORS
 app.use ((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader("Access-Control-Allow-Credentials", "true");
@@ -18,6 +18,7 @@ app.use ((req,res,next) => {
     next();
 });
 
+// Middleware to log date and method of the request
 app.use(function(req, res, next){
     console.log("Request date: " + new Date());
     console.log("Request method: ", req.method)
@@ -25,26 +26,33 @@ app.use(function(req, res, next){
 });
 
 app.use(express.json());
+
+// Serve static files from the /static folder
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
+// Sets the default port
 app.set("port", 3000);
 
 const MongoClient = require("mongodb").MongoClient;
 
 let db;
+// Connect to the database
 MongoClient.connect("mongodb+srv://avrylrodrigues_db_user:LZqc5pTdiCZdpSv7@afterschoolclub.dku2hbd.mongodb.net/", (err, client) => {
     db = client.db("Lessons_DB")
 })
 
+// Root route
 app.get("/", (req, res, next) => {
     res.send("Select a collection, e.g., /collection/messages")
 })
 
+// Middleware to handle collection name in the URL
 app.param("collectionName", (req, res, next, collectionName) => {
     req.collection = db.collection(collectionName)
     return next()
 })
 
+// Gets all the documents in the collection
 app.get("/collection/:collectionName", (req, res, next) => {
     req.collection.find({}).toArray((e, results) => {
         if (e) return next (e)
@@ -52,6 +60,7 @@ app.get("/collection/:collectionName", (req, res, next) => {
     })
 })
 
+// Inserts a new document into a collection
 app.post("/collection/:collectionName", (req, res, next) => {
     req.collection.insert(req.body, (e, results) => {
         if (e) return next (e)
@@ -60,6 +69,8 @@ app.post("/collection/:collectionName", (req, res, next) => {
 })
 
 const ObjectID = require("mongodb").ObjectID;
+
+// Gets a specific document
 app.get("/collection/:collectionName/:id", (req, res, next) => {
     req.collection.findOne({_id: new ObjectID(req.params.id)}, (e, result) => {
         if (e) return next (e)
@@ -67,6 +78,7 @@ app.get("/collection/:collectionName/:id", (req, res, next) => {
     })
 })
 
+// Updates a document
 app.put('/collection/:collectionName/:id', (req, res, next) => {
     req.collection.update(
         {_id: new ObjectID(req.params.id)},
@@ -78,10 +90,12 @@ app.put('/collection/:collectionName/:id', (req, res, next) => {
         });
 });
 
+// Error Handler
 app.use(function(req, res){
     res.status(404);
     res.send("File not Found!");
 });
 
+// Start the server
 const port = process.env.PORT || 3000
 app.listen(port)
